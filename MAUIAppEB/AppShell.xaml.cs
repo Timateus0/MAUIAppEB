c68aa267d83061673b90b0d5771e8689c2cb0f32
Timofey Tereshchenko <data38nur@gmail.com>
2024-12-08 11:52:42 +0300

    Интерфейс
    Добавлен минимальный пользовательский интерфейс

---
+++ MAUIAppEB/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUIAppEB
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MAUIAppEB/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUIAppEB
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
